2593f11ba7dcc557d2ff4bda7d96c5a42ba5dc9b
Edgar Salinas <EdgarHSalinas@gmail.com>
2018-05-20 19:40:21 -0700

    Added bootstrap CDN

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: []
+    };
+  }
+ 
   render() {
     return(
       <div className="App">
-        <h1>Hello, World! </h1>
+      
+      <h1>MovieList</h1>
+    
       </div>
     );
   }
